796879c4a495751769f5bb1463a7307281be72b3
Любомир Лалов <ldlalov@hotmail.com>
2022-01-26 22:02:40 +0200

    08._Magic_Sum

---
+++ 06. Equal Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (numbers.Length==1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            bool found = false;
+            for (int i = 0; i < numbers.Length ; i++)
+            {
+                int sumLeft = 0;
+                int sumRight = 0;
+                int currCell = i;
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += numbers[j];
+                }
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    sumRight += numbers[j];
+                }
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine(i);
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ 08. Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int cmp = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i+1 ; j < numbers.Length; j++)
+                {
+                    if (cmp == numbers[i] + numbers[j])
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
