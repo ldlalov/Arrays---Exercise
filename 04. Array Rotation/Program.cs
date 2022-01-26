90f44826f2d2bc3d5f8b4550f350d4611c8f7c12
Любомир Лалов <ldlalov@hotmail.com>
2022-01-26 17:44:27 +0200

    05._Top_Integers

---
+++ 04. Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] tempArray = new int[array1.Length];
+            int moves = int.Parse(Console.ReadLine());
+            for (int i = 0; i < moves; i++)
+            {
+                int first = array1[0];
+                for (int j = 0; j < tempArray.Length-1; j++)
+                {
+                    tempArray[j] = array1[j+1];
+                }
+                tempArray[array1.Length - 1] = first;
+                array1 = tempArray;
+            }
+            Console.WriteLine("{0}", string.Join(" ", array1));
+        }
+    }
+}
---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int cmp = int.MinValue;
+            int resultCount = 0;
+            int[] result = new int[numbers.Length];
+            for (int i = numbers.Length-1; i >= 0; i--)
+            {
+                    if (numbers[i]>cmp)
+                {
+                    cmp = numbers[i];
+                    result[i] = numbers[i];
+                    resultCount++;
+                }
+            }
+            int[] test = new int[resultCount];
+            int testCount = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i]!=0)
+                {
+                    test[testCount] = result[i];
+                    testCount++;
+                }
+            }
+            Console.WriteLine($"{string.Join(" ", test)}");
+        }
+    }
+}
