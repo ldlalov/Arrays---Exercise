7ab0d0c0840641573831a753ccd891bb774625f9
Любомир Лалов <ldlalov@hotmail.com>
2022-01-26 17:36:36 +0200

    Add project files.

---
+++ 01. Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vagons = int.Parse(Console.ReadLine());
+            int[] train = new int[vagons];
+            int sum = 0;
+            for (int vagon = 0; vagon<vagons;vagon++)    
+            {
+                int passengers = int.Parse(Console.ReadLine());
+                train[vagon] =  passengers;
+                sum += passengers;
+            }
+            Console.WriteLine("{0}", string.Join(" ", train));
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] area1 = Console.ReadLine().Split().ToArray();
+            string[] area2 = Console.ReadLine().Split().ToArray();
+            for (int i = 0; i < area2.Length; i++)
+            {
+                for (int j = 0; j < area1.Length; j++)
+                {
+                    if (area1[j] == area2[i])
+                    {
+                        Console.Write($"{area2[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
