baee231f229f77b681c2b7a6e1be11163dc905c6
Любомир Лалов <ldlalov@hotmail.com>
2022-01-28 14:43:34 +0200

    new Kamino

--- 09. Kamino Factory/Program.cs
+++ 09. Kamino Factory/Program.cs
@@ -11,63 +11,91 @@ namespace _09._Kamino_Factory
             int bestPosition = int.MaxValue;
             int moreOnes = 0;
             int biggestOneCount = 0;
-            int currentSequence = 0;
-            int [] bestsequence = new int[dna];
+            int bestNumSequence = 0;
+            int currentSequence = 1;
+            int[] bestsequence = new int[dna];
             string input = Console.ReadLine();
             while (input != "Clone them!")
             {
-                int oneCount = 0;
-                int onesCount = 0;
-                int position = 0;
+                string newSequence = "";
+                int sumOnes = 0;
+                int couple = 0;
+                int couples = 0;
                 int[] sequence = input.Split("!").Select(int.Parse).ToArray();
+                foreach (int ones in sequence)
+                {
+                    sumOnes += ones;
+                }
                 for (int i = 0; i < sequence.Length; i++)
                 {
-                    if (sequence[i] == 1)
+                    newSequence += sequence[i].ToString();
+                }
+                for (int i = 0; i < input.Length; i++)
+                {
+                        couple = newSequence.IndexOf("11");
+                    bestPosition = couple;
+                    if (couple>0)
                     {
-                        oneCount++;
+                        i = couple + 1;
                     }
                 }
-                    for (int i = 0; i < sequence.Length-1; i++)
-                {
-                    if (sequence[i] == 1 && sequence[i+1] == 1)
-                    {
-                        onesCount++;
-                        position = i;
-                    }
-                }
-                if (onesCount > moreOnes)
-                {
-                    moreOnes = onesCount;
-                    bestPosition = position;
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    currentSequence++;
-                }
-                else if (onesCount == moreOnes && position < bestPosition)
-                {
-                    moreOnes = onesCount;
-                    bestPosition = position;
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    currentSequence++;
-                }
-                else if (onesCount == moreOnes && position == bestPosition && biggestOneCount < oneCount)
-                    {
-                    moreOnes = onesCount;
-                    bestPosition = position;
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    currentSequence++;
-                }
+
+                //int oneCount = 0;
+                //int onesCount = 0;
+                //int position = int.MaxValue;
+                //for (int i = 0; i < sequence.Length; i++)
+                //{
+                //    if (sequence[i] == 1)
+                //    {
+                //        oneCount++;
+                //    }
+                //}
+                //    for (int i = 0; i < sequence.Length-1; i++)
+                //{
+                //    if (sequence[i] == 1 && sequence[i+1] == 1)
+                //    {
+                //        onesCount++;
+                //        if (position==int.MaxValue)
+                //        {
+                //        position = i;
+                //        }
+                //        i++;
+                //    }
+                //}
+                //if (onesCount > moreOnes)
+                //{
+                //    moreOnes = onesCount;
+                //    bestPosition = position;
+                //    bestsequence = sequence;
+                //    biggestOneCount = oneCount;
+                //    bestNumSequence = currentSequence;
+                //}
+                //else if (onesCount == moreOnes && position < bestPosition)
+                //{
+                //    moreOnes = onesCount;
+                //    bestPosition = position;
+                //    bestsequence = sequence;
+                //    biggestOneCount = oneCount;
+                //    bestNumSequence = currentSequence;
+                //}
+                //else if (onesCount == moreOnes && position == bestPosition && biggestOneCount < oneCount)
+                //    {
+                //    moreOnes = onesCount;
+                //    bestPosition = position;
+                //    bestsequence = sequence;
+                //    biggestOneCount = oneCount;
+                //    bestNumSequence = currentSequence;
+                //}
 
 
                 input = Console.ReadLine();
                 if (input != "Clone them!")
                 {
                     sequence = input.Split("!").Select(int.Parse).ToArray();
+                    currentSequence++;
                 }
             }
-            Console.WriteLine($"Best DNA sample {currentSequence} with sum: {biggestOneCount}.");
+            Console.WriteLine($"Best DNA sample {bestNumSequence} with sum: {biggestOneCount}.");
             Console.WriteLine($"{string.Join(" ", bestsequence)}");
         }
     }
