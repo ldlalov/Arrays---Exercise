ebc256dd737eb526f95dec62cbcc581581e7e6c5
Lyubomir Lalov <ldlalov@yahoo.com>
2022-01-31 11:14:58 +0200

    Full Ready!

--- 09. Kamino Factory/Program.cs
+++ 09. Kamino Factory/Program.cs
@@ -8,29 +8,26 @@ namespace _09._Kamino_Factory
         static void Main(string[] args)
         {
             int dna = int.Parse(Console.ReadLine());
-            int bestPosition = int.MaxValue;
+            int bestPosition = 0;
             int moreOnes = 0;
             int biggestOneCount = 0;
-            int bestNumSequence = 0;
+            int bestNumSequence = 1;
             int currentSequence = 1;
             int[] bestsequence = new int[dna];
-            string input = Console.ReadLine();
-            while (input != "Clone them!")
+            string input;
+            while ((input = Console.ReadLine()) != "Clone them!")
             {
                 int sumOnes = 0;
                 int oneCount = 0;
-                int onesCount = 0;
-                int position = int.MaxValue;
-                int[] sequence = input.Split("!").Select(int.Parse).ToArray();
+                int position = 0;
+                int[] sequence = input.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
                 foreach (int ones in sequence)
                 {
-                    if (ones == 1)
-                    {
                         oneCount += ones;
-                    }
                 }
                 int tempSumOnes = 0;
-                for (int i = 0; i < sequence.Length - 1; i++)
+                for (int i = 0; i < sequence.Length; i++)
                 {
                     if (sequence[i] == 1)
                     {
@@ -43,38 +40,33 @@ namespace _09._Kamino_Factory
                     if (tempSumOnes > sumOnes)
                     {
                         sumOnes = tempSumOnes;
-                            position = i;
+                        position = i;
                     }
                 }
-                if (sumOnes > moreOnes)
-                {
-                    moreOnes = sumOnes;
-                    bestPosition = position;
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    bestNumSequence = currentSequence;
-                }
-                else if (sumOnes == moreOnes && position < bestPosition)
-                {
-                    bestPosition = position;
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    bestNumSequence = currentSequence;
-                }
-                else if (sumOnes == moreOnes && position == bestPosition && moreOnes < oneCount)
-                {
-                    bestsequence = sequence;
-                    biggestOneCount = oneCount;
-                    bestNumSequence = currentSequence;
-                }
+                position -= sumOnes-1;
+                    if (sumOnes > moreOnes)
+                    {
+                        moreOnes = sumOnes;
+                        bestPosition = position;
+                        bestsequence = sequence;
+                        biggestOneCount = oneCount;
+                        bestNumSequence = currentSequence;
+                    }
+                    else if (sumOnes == moreOnes && position < bestPosition)
+                    {
+                        bestPosition = position;
+                        bestsequence = sequence;
+                        biggestOneCount = oneCount;
+                        bestNumSequence = currentSequence;
+                    }
+                    else if (sumOnes == moreOnes && position == bestPosition && moreOnes < oneCount)
+                    {
+                        bestsequence = sequence;
+                        biggestOneCount = oneCount;
+                        bestNumSequence = currentSequence;
+                    }
+                currentSequence++;
 
-
-                input = Console.ReadLine();
-                if (input != "Clone them!")
-                {
-                    sequence = input.Split("!").Select(int.Parse).ToArray();
-                    currentSequence++;
-                }
             }
             Console.WriteLine($"Best DNA sample {bestNumSequence} with sum: {biggestOneCount}.");
             Console.WriteLine($"{string.Join(" ", bestsequence)}");
